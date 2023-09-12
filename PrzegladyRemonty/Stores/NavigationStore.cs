f4c7677f7636ca34fe7a1fa093cdc98d72c08568
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2023-09-13 01:01:21 +0200

    NavigationStore

---
+++ PrzegladyRemonty/Stores/NavigationStore.cs
@@ -0,0 +1,22 @@
+﻿using PrzegladyRemonty.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzegladyRemonty.Stores
+{
+    class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+            }
+        }
+    }
+}
