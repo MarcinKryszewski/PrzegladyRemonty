96e66b3bb64e45cf483be96f2b2e1d97131e501a
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2023-10-28 11:05:55 +0200

    PermissionProvider

---
+++ PrzegladyRemonty/Models/Permission.cs
@@ -0,0 +1,21 @@
+﻿namespace PrzegladyRemonty.Models
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PermissionValue { get; set; }
+
+        public Permission(string name, int permissionValue)
+        {
+            Name = name;
+            PermissionValue = permissionValue;
+        }
+        public Permission(int id, string name, int permissionValue)
+        {
+            Id = id;
+            Name = name;
+            PermissionValue = permissionValue;
+        }
+    }
+}
--- PrzegladyRemonty/Services/Providers/PermissionProvider.cs
+++ PrzegladyRemonty/Services/Providers/PermissionProvider.cs
@@ -1,35 +1,121 @@
-﻿using PrzegladyRemonty.Database.DTOs;
-using System;
+﻿#region Usings
+using Dapper;
+using PrzegladyRemonty.Database;
+using PrzegladyRemonty.Database.DTOs;
+using PrzegladyRemonty.Models;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
+#endregion
 
 namespace PrzegladyRemonty.Services.Providers
 {
-    public class PermissionProvider : IDatabaseDTOProvider<PermissionDTO>
+    public class PermissionProvider : IDatabaseDTOProvider<Permission>
     {
-        public void Create(PermissionDTO item)
+        private readonly DatabaseConnectionFactory _dbContextFactory;
+
+        #region SQLCommands
+        private const string _createSQL = @"
+                INSERT INTO
+                permission (Name, PermissionValue)
+                VALUES (@Name, @PermissionValue)
+                ";
+        private const string _deleteSQL = @"
+                DELETE
+                FROM permission
+                WHERE Id = @Id
+                ";
+        private const string _getAllSQL = @"
+                SELECT *
+                FROM permission
+                ";
+        private const string _getOneSQL = @"
+                SELECT *
+                FROM permission
+                WHERE Id = @Id
+                ";
+        private const string _updateSQL = @"
+                UPDATE  permission
+                SET (
+                    Name = @Name,
+                    PermissionValue = @PermissionValue
+                )
+                WHERE Id = @Id
+                ";
+        #endregion
+
+        public PermissionProvider(DatabaseConnectionFactory dbContextFactory)
         {
-            throw new NotImplementedException();
+            _dbContextFactory = dbContextFactory;
         }
 
-        public void Delete(int id)
+        #region CRUD
+        public async void Create(Permission permission)
         {
-            throw new NotImplementedException();
+            using (IDbConnection database = _dbContextFactory.Connect())
+            {
+                object parameters = new
+                {
+                    Name = permission.Name,
+                    PermissionValue = permission.PermissionValue
+                };
+                await database.ExecuteAsync(_createSQL, parameters);
+            }
         }
-
-        public Task<IEnumerable<PermissionDTO>> GetAll()
+        public async void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (IDbConnection database = _dbContextFactory.Connect())
+            {
+                object parameters = new
+                {
+                    Id = id
+                };
+                await database.ExecuteAsync(_deleteSQL, parameters);
+            }
         }
-
-        public PermissionDTO GetById(int id)
+        public async Task<IEnumerable<Permission>> GetAll()
         {
-            throw new NotImplementedException();
+            using (IDbConnection database = _dbContextFactory.Connect())
+            {
+                IEnumerable<PermissionDTO> permissionDTOs = await database.QueryAsync<PermissionDTO>(_getAllSQL);
+                return permissionDTOs.Select(ToPermission);
+            }
         }
-
-        public void Update(PermissionDTO item)
+        public Permission GetById(int id)
         {
-            throw new NotImplementedException();
+            using (IDbConnection database = _dbContextFactory.Connect())
+            {
+                object parameters = new
+                {
+                    Id = id
+                };
+                PermissionDTO permissionDTO = database.QuerySingleOrDefault<PermissionDTO>(_getOneSQL, parameters);
+                return ToPermission(permissionDTO);
+            }
+        }
+        public async void Update(Permission permission)
+        {
+            using (IDbConnection database = _dbContextFactory.Connect())
+            {
+                object parameters = new
+                {
+                    Id = permission.Id,
+                    Name = permission.Name,
+                    PermissionValue = permission.PermissionValue
+
+                };
+                await database.ExecuteAsync(_updateSQL, parameters);
+            }
+        }
+        #endregion
+
+        private static Permission ToPermission(PermissionDTO permissionDTO)
+        {
+            return new Permission(
+                permissionDTO.Id,
+                permissionDTO.Name,
+                permissionDTO.PermissionValue);
         }
     }
 }
